07de5844881213871db090d35e174450a7892d2e
jasperthon <japer.guerrero1996@gmail.com>
2016-07-10 13:57:55 -0700

    Initial commit

---
+++ djbasics/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\JasperCrashCourseDjango_2016.07.10\django_basics\djbasics\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
